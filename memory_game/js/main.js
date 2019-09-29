f2877f292f7e82dd195c78e7f8a225586c8194fd
Charlotte Thomas <charlottethomas@unknown.home>
2019-09-29 15:12:36 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,26 +1,36 @@
 console.log('Up and running!');
 
-/* console.log('User flipped' + cardOne);
-console.log('User flipped' + cardThree); */
-
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped queen');
-console.log('User flipped king');
-
+function checkForMatch() {
 if (cardsInPlay.length === 2) {
-	console.log('Two cards selected');
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert('You found a match!');
-	}else {
-	alert('Sorry, try again.');
-	}
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
-//delete two cards selected log
+}
+}
+
+function flipCard(cardID) {
+console.log('User flipped '+ cards[cardID]);
+cardsInPlay.push(cards[cardID]);
+checkForMatch();
+};
+
+//or could put .length === 2 before checkForMatch?
+
+flipCard(0);
+flipCard(2);
+
+/* 
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+alert('You found a match!');
+}else {
+ alert('Sorry, try again.');
+ }
+} 
+*/
+
